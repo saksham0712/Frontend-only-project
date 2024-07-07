e310f62f561968403b7e4684a3e91b1a2659193d
Saksham Verma <sakshamverma626@gmail.com>
2024-07-08 01:13:28 +0530

    adding Footer

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -9,6 +9,7 @@ import one5 from '../assets/OurClients/05.svg'
 import one6 from '../assets/OurClients/06.svg'
 import one7 from '../assets/OurClients/07.svg'
 import arrow from '../assets/Section/arrow.svg'
+import arrow1 from '../assets/Section/Section8/arrow.svg'
 import one from '../assets/Section/Section8/one.svg'
 import two from '../assets/Section/Section8/two.svg'
 import three from '../assets/Section/Section8/three.svg'
@@ -35,7 +36,7 @@ const Section = () => {
 
 
             {/* this is section 7 */}
-            <div className="bg-slate-100 h-[110vh] md:h-[90vh] w-full flex flex-col md:flex-row justify-around items-center">
+            <div className="bg-slate-100 h-auto my-6 py-4 md:h-[90vh] w-auto flex flex-col md:flex-row justify-around items-center">
                 <div className="left"><div className="secure w-[60vw] md:w-auto"><img src={section7} alt="hero image" /></div></div>
                 <div className="right w-auto md:w-[60vw] flex flex-col items-center md:items-start gap-6">
                     <div className='text-center md:text-start'>
@@ -65,20 +66,21 @@ const Section = () => {
 
 
             {/* section 8 */}
-            <div className='w-full bg-white text-center flex flex-col justify-items-center gap-4'>
+            <div className='h-auto my-5 md:h-[100vh] w-full bg-white text-center flex flex-col justify-items-center gap-4'>
                 <h3 className='text-gray-700 text-3xl md:text-5xl font-semibold mt-10'>Caring is the new marketing</h3>
-                <div className=' m-auto w-auto md:w-[50vw]'>
+                <div className=' mx-auto my-4 w-auto md:w-[50vw]'>
 
                     <p className='text-gray-700 text-center'>The Nexcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center justify-center mt-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center justify-center">
+
                     <div className='flex flex-col relative items-center w-[80%] min-w-[26vw] m-auto py-6'>
                         <img className='w-auto md:w-auto' src={one} alt="memb" />
-                        <div className='absolute flex flex-col items-center gap-4 bg-slate-100 text-center border rounded-lg bottom-[-5px] w-5/6'>
-                            <h3 className='text-gray-700 text-large md:text-2xl font-semibold mt-5'>Creating Streamlined Safeguarding Processes with OneRen</h3>
+                        <div className='absolute flex flex-col items-center gap-4 bg-slate-100 text-center border rounded-lg bottom-[-5px] w-5/6 shadow-md'>
+                            <h3 className='text-gray-700 text-large md:text-large font-semibold mt-5'>Creating Streamlined Safeguarding Processes with OneRen</h3>
                             <div className='flex gap-2'>
 
-                                <h3 className='text-green-600 text-center'>Meet all customers</h3>
+                                <h3 className='text-green-600 text-center'>Read More</h3>
                                 <img src={arrow} className='w-6 h-6' alt="arrow" />
                             </div>
 
@@ -86,11 +88,11 @@ const Section = () => {
                     </div>
                     <div className='flex flex-col relative items-center w-[80%] min-w-[26vw] m-auto py-6'>
                         <img className='w-auto md:w-auto' src={two} alt="memb" />
-                        <div className='absolute flex flex-col items-center gap-4 bg-slate-100 text-center border rounded-lg bottom-[-5px] w-5/6'>
-                            <h3 className='text-gray-700 text-large md:text-2xl font-semibold mt-5'>Creating Streamlined Safeguarding Processes with OneRen</h3>
+                        <div className='absolute flex flex-col items-center gap-4 bg-slate-100 text-center border rounded-lg bottom-[-5px] w-5/6 shadow-md'>
+                            <h3 className='text-gray-700 text-large md:text-large font-semibold mt-5'>What are your safeguarding responsibilities and how can you manage them?</h3>
                             <div className='flex gap-2'>
 
-                                <h3 className='text-green-600 text-center'>Meet all customers</h3>
+                                <h3 className='text-green-600 text-center'>Read More</h3>
                                 <img src={arrow} className='w-6 h-6' alt="arrow" />
                             </div>
 
@@ -98,17 +100,33 @@ const Section = () => {
                     </div>
                     <div className='flex flex-col relative items-center w-[80%] min-w-[26vw] m-auto py-6'>
                         <img className='w-auto md:w-auto' src={three} alt="memb" />
-                        <div className='absolute flex flex-col items-center gap-4 bg-slate-100 text-center border rounded-lg bottom-[-5px] w-5/6'>
-                            <h3 className='text-gray-700 text-large md:text-2xl font-semibold mt-5'>Creating Streamlined Safeguarding Processes with OneRen</h3>
+                        <div className='absolute flex flex-col items-center gap-4 bg-slate-100 text-center border rounded-lg bottom-[-5px] w-5/6 shadow-md'>
+                            <h3 className='text-gray-700 text-large md:text-large font-semibold mt-5'>Revamping the Membership Model with Triathlon Australia</h3>
                             <div className='flex gap-2'>
 
-                                <h3 className='text-green-600 text-center'>Meet all customers</h3>
+                                <h3 className='text-green-600 text-center'>Read More</h3>
                                 <img src={arrow} className='w-6 h-6' alt="arrow" />
                             </div>
 
                         </div>
                     </div>
-                    
+
+                </div>
+            </div>
+
+
+            {/* Section 9 */}
+
+            <div className='bg-slate-100 flex flex-col gap-4 flex-wrap justify-center items-center h-auto md:h-auto'>
+                <div className="left flex flex-col gap-3 text-center mt-4 mb-4 md:text-start w-[80vw]">
+                    <h1 className='text-gray-500 text-5xl md:text-8xl mt-4 font-semibold text-center'>Pellentesque suscipit fringilla libero eu.</h1>
+                </div>
+                <div>
+                    <button type="button" className=" flex gap-2 items-center focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-sm text-lg px-5 py-2.5 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                        <p>Get Demo</p>
+                        <img src={arrow1} className='w-6 h-6 items-center' alt="arrow" />
+                    </button>
+
                 </div>
             </div>
         </>
