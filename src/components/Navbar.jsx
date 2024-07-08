83f16fc60a12fe6ba3b2d1b46249b8d1b6b22ae4
Saksham Verma <sakshamverma626@gmail.com>
2024-07-08 22:33:52 +0530

    now i am sure it is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 hidden md:block focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Login</button>
 
           </div>
-          <button type="button" className='block md:hidden' onClick={(e) => { toggleNavItems() }}><svg width="40px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <button type="button" className='block md:hidden filter dark:stroke-slate-500' onClick={(e) => { toggleNavItems() }}><svg width="40px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z" fill="#000000" />
           </svg></button>
 
